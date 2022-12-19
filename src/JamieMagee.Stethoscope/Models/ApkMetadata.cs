5d1bcebc5a03423a3fd77a1f1f4c494267d506d8
Jamie Magee <jamie.magee@gmail.com>
2022-12-19 14:14:46 -0800

    Begin APK parser

---
+++ src/JamieMagee.Stethoscope/Models/ApkMetadata.cs
@@ -0,0 +1,55 @@
+﻿namespace JamieMagee.Stethoscope.Models;
+
+using System.Text.Json.Serialization;
+
+/// <summary>
+/// ApkMetadata represents all captured data for a Alpine DB package entry.
+/// See the following sources for more information:
+/// - https://wiki.alpinelinux.org/wiki/Apk_spec
+/// - https://git.alpinelinux.org/apk-tools/tree/src/package.c
+/// - https://git.alpinelinux.org/apk-tools/tree/src/database.c
+/// </summary>
+public sealed record ApkMetadata
+{
+    [JsonPropertyName("package")]
+    public string Package { get; set; }
+
+    [JsonPropertyName("originPackage")]
+    public string OriginPackage { get; set; }
+
+    [JsonPropertyName("maintainer")]
+    public string Maintainer { get; set; }
+
+    [JsonPropertyName("version")]
+    public string Version { get; set; }
+
+    [JsonPropertyName("license")]
+    public string License { get; set; }
+
+    [JsonPropertyName("architecture")]
+    public string Architecture { get; set; }
+
+    [JsonPropertyName("url")]
+    public string Url { get; set; }
+
+    [JsonPropertyName("description")]
+    public string Description { get; set; }
+
+    [JsonPropertyName("size")]
+    public long Size { get; set; }
+
+    [JsonPropertyName("installedSize")]
+    public long InstalledSize { get; set; }
+
+    [JsonPropertyName("dependencies")]
+    public string[] Dependencies { get; set; }
+
+    [JsonPropertyName("provides")]
+    public string[] Provides { get; set; }
+
+    [JsonPropertyName("checksum")]
+    public string Checksum { get; set; }
+
+    [JsonPropertyName("gitCommit")]
+    public string GitCommit { get; set; }
+}
---
+++ src/JamieMagee.Stethoscope/Parsers/ApkDatabaseParser.cs
@@ -0,0 +1,37 @@
+﻿namespace JamieMagee.Stethoscope.Parsers;
+
+using JamieMagee.Stethoscope.Models;
+
+public static class ApkDatabaseParser
+{
+    public static async Task<IEnumerable<ApkMetadata>> ParseAsync(StreamReader reader)
+    {
+        var apks = new List<ApkMetadata>();
+
+        while (!reader.EndOfStream)
+        {
+            var line = await reader.ReadLineAsync();
+            var apk = new ApkMetadata();
+            while (line != string.Empty)
+            {
+                switch (line)
+                {
+                    case { } when line.StartsWith("P", StringComparison.Ordinal):
+                        apk.Package = line[2..];
+                        break;
+                    case { } when line.StartsWith("V", StringComparison.Ordinal):
+                        apk.Version = line[2..];
+                        break;
+                    default:
+                        break;
+                }
+
+                line = await reader.ReadLineAsync();
+            }
+
+            apks.Add(apk);
+        }
+
+        return apks;
+    }
+}
